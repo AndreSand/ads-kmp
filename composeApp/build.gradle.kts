8b605a2e3fb1446bc30ba73f7df0053dae4fe804
Andres <android.sjsu@gmail.com>
2025-01-04 01:52:56 -0800

    Add Android AdMob Ads SDK configuration. https://developers.google.com/admob/android/quick-start

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -39,6 +39,7 @@ kotlin {
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
+            implementation(libs.play.services.ads)
         }
         commonMain.dependencies {
             implementation(compose.runtime)
--- composeApp/src/androidMain/kotlin/com/sandovalandres/kmp/ads/AdMobBanner.android.kt
+++ composeApp/src/androidMain/kotlin/com/sandovalandres/kmp/ads/AdMobBanner.android.kt
@@ -1,8 +1,23 @@
 package com.sandovalandres.kmp.ads
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
 
 @Composable
 actual fun AdMobBanner(modifier: Modifier) {
+    AndroidView(
+        modifier = modifier.fillMaxWidth(),
+        factory = { context ->
+            AdView(context).apply {
+                setAdSize(AdSize.FULL_BANNER)
+                adUnitId = "ca-app-pub-3940256099942544/9214589741" // Replace with your ad unit ID
+                loadAd(AdRequest.Builder().build())
+            }
+        }
+    )
 }
